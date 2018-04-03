f794042899808358522b64c281904d48c8ccac5e
Nate <Natmcnair2012@gmail.com>
2018-04-03 11:48:17 -0400

    Finish Toy App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render htm: "hello, world!"
-  end 
+    render html: "hello, world!"
+  end
 end
